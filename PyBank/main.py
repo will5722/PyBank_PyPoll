db31d7a1b92f9040d368758bb01170c2f94ca00b
Willam Lubenow <wkl22@humboldt.edu>
2021-08-24 15:33:59 -0700

    add path to budget data, created csvreader, started adding variables/parameters for what needs to be found

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,2 +1,15 @@
-#PyBank script
+import os
+import csv
+
+#path to budget data
+budget_csv = os.path.join('Resources', 'budget_data.csv')
+#Read through data
+with open(budget_csv, 'r') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+#Track what we are looking for in the data
+    TotalMonths = 0
+    NetTotal = 0
+
+
 
