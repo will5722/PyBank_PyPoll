cd34e82f0e7cef454d3c7db08c6223999b4caac6
Willam Lubenow <wkl22@humboldt.edu>
2021-08-24 16:57:11 -0700

    deciding what to do with PrevPL

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,10 +6,30 @@ budget_csv = os.path.join('Resources', 'budget_data.csv')
 #Read through data
 with open(budget_csv, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
+    next(csvreader, None)
 
-#Track what we are looking for in the data
+#Track what we are looking for in the data, may need to make others once loop is made
     TotalMonths = 0
+    Month = []
     NetTotal = 0
+    TotalRevChange = []
+
+    #Set at 0 since no previous month at first, set again in loop for previous month
+    PrevPL = 0
+   
+    
+
+    for row in csvreader:
+        #Start with easiest problems first, get total months and total profit/loss
+        #Convert data from file to integer
+        TotalMonths += 1
+        NetTotal += int(row[1])
+        #Find the change between each month first, then find average outside of loop
+        PLchange = int(row[1]) - int(PrevPL)
+        #set previous profit/loss for the next iteration
+        PrevPL = row[1]
+
+
 
 
 
