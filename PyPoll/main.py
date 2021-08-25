ce910fd6bd42821a616a1f4c21f5f6a774f18051
Willam Lubenow <wkl22@humboldt.edu>
2021-08-25 11:27:33 -0700

    fixed csvreader

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,3 +1,12 @@
 import os
 import csv
 
+#Path to election data
+election_csv = os.path.join('Resources', 'election_data.csv')
+#Open and read data
+with open(election_csv, 'r') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    next(csvreader, None)
+
+
+    
