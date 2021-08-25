2cb75dcdce8aee2ef3644acbfd2a9d4ff9069764
Willam Lubenow <wkl22@humboldt.edu>
2021-08-25 11:46:25 -0700

    added total votes counter and empty lists

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -8,5 +8,16 @@ with open(election_csv, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     next(csvreader, None)
 
+#Set variables/parameters to track
+    TotalVotes = 0
+    Candidates = []
+    CandidateVotes = []
+
+
+
+    for row in csvreader:
+        Totalvotes =+ 1
+        Candidates.append(row[2])
+        
 
     
